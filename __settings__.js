584957ab7c8111bd866d3e0eedfbe2a5358b5b35
Aston <aston.lviv@gmail.com>
2025-01-11 19:38:16 +0200

    initial commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2149774.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 210280345, 210281454, 210282257 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/210281967/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/210281968/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/210281966/1/ammo.js', 'preload' : true},
+];
